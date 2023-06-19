9e09e0b0639b6da4b40850e0276e7f572713c864
Tomas Harkema <tomas@harkema.io>
2023-06-19 11:30:09 +0200

    introduce swiftlint

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "RawJson",
-//    platforms: [.macOS("13.3"), .iOS("16.4")],
+    platforms: [.macOS(.v12), .iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["RawJson"]),
     ],
     dependencies: [
-         .package(url: "https://github.com/apple/swift-foundation.git", branch: "main")
+        .package(url: "https://github.com/realm/SwiftLint.git", .upToNextMinor(from: "0.52.2")),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
@@ -21,7 +21,9 @@ let package = Package(
         .target(
             name: "RawJson",
             dependencies: [
-                // .product(name: "FoundationPreview", package: "swift-foundation")
+            ],
+            plugins: [
+                .plugin(name: "SwiftLintPlugin", package: "SwiftLint"),
             ]
         ),
         .testTarget(
